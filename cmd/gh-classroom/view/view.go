8852a83677daf4b9fe4094c57ab45e169f57048f
Ashley <saturn226@github.com>
2023-03-09 15:26:12 -0500

    added flags to command view help

--- cmd/gh-classroom/view/view.go
+++ cmd/gh-classroom/view/view.go
@@ -11,9 +11,15 @@ func NewCmdView(f *cmdutil.Factory) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "view",
 		Short: "Show the details of a Classroom",
+		Long: `Display the classroom ID, classroom slug, title and other information about a classroom.
+With "--web", open the classroom in a browser instead
+For more information about output formatting flags, see "gh help"`,
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("View Classroom")
 		},
 	}
+
+	cmd.Flags().BoolP("web", "w", false, "Open classroom in the browser")
+	cmd.Example = `gh classroom view 4 --web`
 	return cmd
 }
