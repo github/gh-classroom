3753fc3f77a11802a361467aeeb0716223cd7833
Steven Nunez <octosteve@github.com>
2023-03-09 16:23:47 -0500

    WIP Add Web and some 'styling'

--- cmd/gh-classroom/assignments/assignments.go
+++ cmd/gh-classroom/assignments/assignments.go
@@ -2,40 +2,19 @@ package assignments
 
 import (
 	"fmt"
-	"github.com/cli/go-gh"
-	"github.com/cli/go-gh/pkg/tableprinter"
-	"github.com/spf13/cobra"
 	"log"
 	"os"
 	"strconv"
+
+	"github.com/cli/cli/v2/pkg/iostreams"
+	"github.com/cli/go-gh"
+	"github.com/cli/go-gh/pkg/browser"
+	"github.com/cli/go-gh/pkg/tableprinter"
+	"github.com/cli/go-gh/pkg/term"
+	"github.com/cli/go-gh/pkg/text"
+	"github.com/github/gh-classroom/pkg/classroom"
+	"github.com/spf13/cobra"
 )
-
-type Assignment struct {
-	Id                          int            `json:"id" :"id"`
-	PublicRepo                  bool           `json:"public_repo" :"public___repo"`
-	Title                       string         `json:"title" :"title"`
-	AssignmentType              string         `json:"type" :"assignment___type"`
-	InviteLink                  string         `json:"invite_link" :"invite___link"`
-	InvitationsEnabled          bool           `json:"invitations_enabled" :"invitations___enabled"`
-	Slug                        string         `json:"slug" :"slug"`
-	StudentsAreRepoAdmins       bool           `json:"students_are_repo_admins" :"students___are___repo___admins"`
-	FeedbackPullRequestsEnabled bool           `json:"feedback_pull_requests_enabled" :"feedback___pull___requests___enabled"`
-	MaxTeams                    int            `json:"max_teams" :"max___teams"`
-	MaxMembers                  int            `json:"max_members" :"max___members"`
-	Editor                      string         `json:"editor" :"editor"`
-	Accepted                    int            `json:"accepted" :"accepted"`
-	Submissions                 int            `json:"submissions" :"submissions"`
-	Passing                     int            `json:"passing" :"passing"`
-	Language                    string         `json:"language" :"language"`
-	Classroom                   ShortClassroom `json:"classroom" :"classroom"`
-}
-
-type ShortClassroom struct {
-	Id       int    `json:"id" :"id"`
-	Name     string `json:"name" :"name"`
-	archived bool   `json:"archived" :"archived"`
-	url      string `json:"url" :"url"`
-}
 
 func NewCmdAssignments() *cobra.Command {
 	var web bool
@@ -48,6 +27,10 @@ func NewCmdAssignments() *cobra.Command {
 		Short: "Display a list of assignments for a classroom.",
 		Long:  "Display a list of assignments for a classroom.",
 		Run: func(cmd *cobra.Command, args []string) {
+			term := term.FromEnv()
+			io := iostreams.System()
+			cs := io.ColorScheme()
+
 			if classroomID == 0 {
 				log.Fatal("Missing classroom ID")
 			}
@@ -56,31 +39,58 @@ func NewCmdAssignments() *cobra.Command {
 				log.Fatal(err)
 			}
 
-			var response []Assignment
+			var response []classroom.Assignment
 			err = client.Get(fmt.Sprintf("classrooms/%v/assignments", classroomID), &response)
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			fmt.Println(classroomID)
-			t := tableprinter.New(os.Stdout, true, 14)
-			t.AddField("ID", tableprinter.WithTruncate(nil))
-			t.AddField("Title", tableprinter.WithTruncate(nil))
-			t.AddField("Repo Visibility", tableprinter.WithTruncate(nil))
-			t.EndRow()
+			assignmentList := classroom.NewAssignmentList(response)
+
+			fmt.Println(assignmentListSummary(assignmentList, cs))
+
+			t := tableprinter.New(os.Stdout, term.IsTerminalOutput(), 14)
+
+			if web {
+				if term.IsTerminalOutput() {
+					fmt.Fprintln(io.ErrOut, "Opening in your browser.")
+				}
+				browser := browser.New("", io.Out, io.ErrOut)
+				browser.Browse(assignmentList.Url())
+				return
+			}
+
 			for _, assignment := range response {
-				t.AddField(strconv.Itoa(assignment.Id), tableprinter.WithTruncate(nil))
+				t.AddField(cs.Green(strconv.Itoa(assignment.Id)), tableprinter.WithTruncate(nil))
 				t.AddField(assignment.Title, tableprinter.WithTruncate(nil))
-				t.AddField(strconv.FormatBool(assignment.PublicRepo))
+				t.AddField(cs.Gray(strconv.FormatBool(assignment.PublicRepo)), tableprinter.WithTruncate(nil))
 				t.EndRow()
 			}
 			t.Render()
 		},
 	}
-	cmd.Flags().BoolVarP(&web, "web", "", false, "Open the assignment list in a browser")
-	cmd.Flags().IntVarP(&page, "page", "", 1, "Page number")
-	cmd.Flags().IntVarP(&perPage, "per-page", "", 30, "Number of assignments per page")
-	cmd.Flags().IntVarP(&classroomID, "classroom-id", "", 0, "ID of the classroom")
+	cmd.Flags().BoolVar(&web, "web", false, "Open the assignment list in a browser")
+	cmd.Flags().IntVar(&page, "page", 1, "Page number")
+	cmd.Flags().IntVar(&perPage, "per-page", 30, "Number of assignments per page")
+	cmd.Flags().IntVarP(&classroomID, "classroom-id", "c", 0, "ID of the classroom")
 
 	return cmd
+}
+
+func colorForVisibility(public bool) string {
+	if public {
+		return "green"
+	}
+	return "red"
+}
+
+func assignmentListSummary(a classroom.AssignmentList, cs *iostreams.ColorScheme) string {
+	switch a.Count {
+	case 0:
+		return fmt.Sprintf("No assignments for %v\n", cs.Blue(a.Classroom.Name))
+	case 1:
+		return fmt.Sprintf("%v for %v (id: %v)\n", text.Pluralize(a.Count, "Assignment"), cs.Blue(a.Classroom.Name))
+	default:
+		return fmt.Sprintf("%v for %v\n", text.Pluralize(a.Count, "Assignment"), cs.Blue(a.Classroom.Name))
+	}
 }
