2e93b653f9b9950aa2f637e2fcb88988fdf836c7
Steven Nunez <octosteve@github.com>
2023-03-09 18:27:54 -0500

    Move tests and fix main

--- cmd/gh-classroom/main_test.go
+++ cmd/gh-classroom/main_test.go
@@ -1,17 +1,1 @@
 package main
-
-import (
-	"bytes"
-	"github.com/stretchr/testify/assert"
-	"testing"
-)
-
-func TestAssignmentsCommandShowsHelp(t *testing.T) {
-	actual := bytes.Buffer{}
-	expected := "Display a list of assignments for a classroom.\n\nUsage:\n  classroom assignments [flags]\n\nFlags:\n  -c, --classroom-id int   ID of the classroom\n  -h, --help               help for assignments\n      --page int           Page number (default 1)\n      --per-page int       Number of assignments per page (default 30)\n      --web                Open the assignment list in a browser\n"
-	cmd := rootCmd()
-	cmd.SetOut(&actual)
-	cmd.SetArgs([]string{"assignments", "--help"})
-	cmd.Execute()
-	assert.Equal(t, expected, actual.String())
-}
