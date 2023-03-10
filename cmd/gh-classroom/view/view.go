5186cb42119e8d5f667deba93d511bc1fdc346bb
Ashley <saturn226@github.com>
2023-03-10 13:59:56 -0500

    seperated the table printing to its own function

--- cmd/gh-classroom/view/view.go
+++ cmd/gh-classroom/view/view.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 
 	"github.com/cli/cli/v2/pkg/cmdutil"
-	"log"
-	"github.com/spf13/cobra"
-	"github.com/cli/go-gh"
-    "github.com/cli/go-gh/pkg/term"
-	"github.com/cli/go-gh/pkg/browser"
 	"github.com/cli/cli/v2/pkg/iostreams"
+	"github.com/cli/go-gh"
+	"github.com/cli/go-gh/pkg/browser"
+	"github.com/cli/go-gh/pkg/tableprinter"
+	"github.com/cli/go-gh/pkg/term"
 	"github.com/github/gh-classroom/pkg/classroom"
-	// "github.com/cli/go-gh/pkg/tableprinter"
-	// "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
+	"log"
+	"strconv"
 )
 
 type Classroom struct {
-	ID int `json:"id"`
-	Slug string `json:"slug"`
+	ID    int    `json:"id"`
+	Slug  string `json:"slug"`
 	Title string `json:"title"`
-	Url string `json:"url"`
+	Url   string `json:"url"`
 }
 
 func NewCmdView(f *cmdutil.Factory) *cobra.Command {
@@ -28,13 +28,13 @@ func NewCmdView(f *cmdutil.Factory) *cobra.Command {
 	term := term.FromEnv()
 	io := iostreams.System()
 	cmd := &cobra.Command{
-		Use:   "view",
-		Example: `$ gh classroom view 4 --web`,
-		Short: "Show the details of a Classroom",
+		Use:     "view",
+		Example: `$ gh classroom view -c 4876 --web`,
+		Short:   "Show the details of a Classroom",
 		Long: `Display the classroom ID, classroom slug, title and other information about a classroom.
 With "--web", open the classroom in a browser instead
 For more information about output formatting flags, see "gh help"`,
-		Run: func(cmd *cobra.Command, args []string){
+		Run: func(cmd *cobra.Command, args []string) {
 			client, err := gh.RESTClient(nil)
 			if classroomID == 0 {
 				log.Fatal("Missing classroom ID. Try again")
@@ -46,11 +46,10 @@ For more information about output formatting flags, see "gh help"`,
 
 			response, err := classroom.GetClassroom(client, classroomID)
 
-			
-			// err = client.Get(fmt.Sprintf("classrooms/%v", classroomID), &response)
+			fmt.Println("CLASSROOM INFORMATION")
+			t := tableprinter.New(cmd.OutOrStdout(), true, 14)
+			PrintTable(response, t)
 
-			fmt.Println("Classroom View")
-		
 			if web {
 				if term.IsTerminalOutput() {
 					fmt.Fprintln(io.ErrOut, "Opening classroom your browser...")
@@ -62,7 +61,25 @@ For more information about output formatting flags, see "gh help"`,
 		},
 	}
 
-	cmd.Flags().BoolVar(&web, "web", false, "Open classroom in the browser")
+	cmd.Flags().BoolVarP(&web, "web", "w", false, "Open classroom in the browser")
 	cmd.Flags().IntVarP(&classroomID, "classroom-id", "c", 0, "ID of the classroom")
 	return cmd
 }
+
+func PrintTable(response classroom.ShortClassroom, t tableprinter.TablePrinter) {
+	t.AddField("ID", tableprinter.WithTruncate(nil))
+	t.AddField("Name", tableprinter.WithTruncate(nil))
+	t.AddField("Archived", tableprinter.WithTruncate(nil))
+	t.AddField("URL", tableprinter.WithTruncate(nil))
+	t.EndRow()
+
+	t.AddField(strconv.Itoa(response.Id), tableprinter.WithTruncate(nil))
+	t.AddField(response.Name, tableprinter.WithTruncate(nil))
+	t.AddField(strconv.FormatBool(response.Archived))
+	t.AddField(response.Url, tableprinter.WithTruncate(nil))
+	t.EndRow()
+
+	t.Render()
+}
+
+
